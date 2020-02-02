f734777073407f456d5a606d648203011aa9f81f
Onyimatics <ezikeonyinyefavour@gmail.com>
2020-02-02 18:11:10 +0100

    angular-material-demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from  '@angular/router';
+import { MyMaterialModule } from  './material.module';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomerCreateComponent } from './customer-create/customer-create.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { ErrorComponent } from './error.component';
+
+const  appRoutes:  Routes  = [
+  { path:  'customer-list', component:  CustomerListComponent },
+  { path:  'customer-create', component:  CustomerCreateComponent },
+  {
+  path:  'login',
+  component:  LoginComponent
+  },
+  { path:  '',
+  redirectTo:  '/login',
+  pathMatch:  'full'
+  },
+  ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CustomerListComponent,
+    CustomerCreateComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    MyMaterialModule,
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+    ),
   ],
+  entryComponents: [ErrorComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
